e0d098ed22614fccbd6153142f825f02a185ca4e
caixw <xnotepad@gmail.com>
2017-05-25 22:36:03 +0800

    为 main 添加一些参数设置

--- main.go
+++ main.go
@@ -5,14 +5,40 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/caixw/go-http-routes-testing/data"
 )
 
 // 数据存放目录
-const dataDir = "./docs/data"
+const (
+	docsDir = "./docs"
+	dataDir = "./docs/data"
+)
+
+const version = "0.1.1+20170525"
+
+// 默认的输出通首
+var output = os.Stdout
 
 func main() {
-	data.JSON(dataDir, os.Stdout)
+	typ := flag.String("type", "html", "指定输出的数据类型，可以是 HTML 或是 JSON")
+	v := flag.Bool("v", false, "显示版本号")
+
+	if *v {
+		fmt.Println("version:", version)
+		return
+	}
+
+	switch strings.ToLower(*typ) {
+	case "html":
+		data.HTML(docsDir, output)
+	case "json":
+		data.JSON(dataDir, output)
+	default:
+		panic("无效的 type 值")
+	}
 }
