e0cdbfa7780d51ea346263e4a6c7ff276143bea1
caixw <xnotepad@gmail.com>
2021-11-09 03:29:32 +0800

    feat: 添加新的路由项

---
+++ apis/long.go
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+package apis
+
+import "net/http"
+
+func init() {
+	long.init()
+}
+
+var long = &Collection{
+	Name: "Long Routes",
+	Desc: "路径很长的路由项",
+	APIS: []*API{
+		{Method: http.MethodGet, Brace: "/loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog/{p1}/loooooooooooooooooooooooooooooooooooooooogooooooooooooooooooooooooooooooog/{p2}.html"},
+		{Method: http.MethodGet, Brace: "/loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog/{p1}/loooooooooooooooooooooooooooooooooooooooogooooooooooooooooooooooooooooooog/{p2}.html"},
+		{Method: http.MethodPut, Brace: "/looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog/{p1}/loooooooooooooooooooooooooooooooooooooooogooooooooooooooooooooooooooooooog/{p2}.html"},
+		{Method: http.MethodPut, Brace: "/looooooooooooooooooooooooooooooooooogooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog/{p1}/looooooooooooooogoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooogooooooooooooooooooooooooooooooog/{p2}.html"},
+		{Method: http.MethodPut, Brace: "/looooooooooooooooooooooooooooooooooogooooooooooooooogooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog/{p1}/loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooogoooooooooooooooooooooooogooooooooooooooooooooooooooooooog/{p2}.html"},
+		{Method: http.MethodGet, Brace: "/looooooooooooooooooooooooooooooooooogooooooooooooooogooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog/{p1}/looooooooooooooogoooooooooooooooooooooooogooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog/{p2}.html"},
+		{Method: http.MethodGet, Brace: "/looooooooooooooooooooooooooooooooooogooooooooooooooogoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog/{p1}/looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooogoooooooooooogooooooooooogooooooooooooooooooooooooooooooog/{p2}.html"},
+		{Method: http.MethodGet, Brace: "/loooooooooooogoooooooooooooooooooooogooooooooooooooogoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog/{p1}/looooooooooooooogoooooooooooogooooooooooogooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog/{p2}.html"},
+		{Method: http.MethodPut, Brace: "/loooooooooooogoooooooooooooooooooooogooooooooooooooogooooooooooooooooooooooooooooooooooooooooooooooooooooooooog/{p1}/looooooooooooooogoooooooooooogooooooooooogoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog/{p2}.html"},
+		{Method: http.MethodPost, Brace: "/loooooooooooogoooooooooooooooooooooogooooooooooooooogooooooooooooooooooooooooooooooooooooooooooog/{p1}/looooooooooooooogoooooooooooogooooooooooogooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog/{p2}.html"},
+		{Method: http.MethodPost, Brace: "/loooooooooooogoooooooooooooooooooooogooooooooooooooogoooooooooooooooooooooooooooooooooog/{p1}/looooooooooooooogoooooooooooogooooooooooogoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog.html"},
+		{Method: http.MethodPost, Brace: "/loooooooooooogoooooooooooooooooooooooooooooooooooooooooooooogoooooogoooooooogoooooooooooooooooooog/{p1}/looooooooooooooogoooooooooooogooooooooooogoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog.html"},
+		{Method: http.MethodPost, Brace: "/loooooooooooogooooooooooooooooooooooooooooooooogoooooogoooooooogoooooooooooooooooooog/{p1}/loooooogoooooooogoooooooooooogooooooooooogooooooooooooooooooooooooooooooooooooooooooooooooooog.html"},
+	},
+}
