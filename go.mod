b5d3488c1aa9061c8d4c31d8b00c503092b39467
caixw <xnotepad@gmail.com>
2021-11-27 04:00:42 +0800

    feat(routers): 添加 fasthttp-router

--- go.mod
+++ go.mod
@@ -2,13 +2,19 @@ module github.com/caixw/go-http-routers-testing
 
 require (
 	github.com/dimfeld/httptreemux/v5 v5.4.0
+	github.com/fasthttp/router v1.4.4
 	github.com/gorilla/mux v1.8.0
 	github.com/issue9/mux/v5 v5.0.0
+	github.com/valyala/fasthttp v1.31.0
 )
 
 require (
+	github.com/andybalholm/brotli v1.0.2 // indirect
 	github.com/issue9/errwrap v0.2.0 // indirect
 	github.com/issue9/sliceutil v0.7.0 // indirect
+	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/savsgio/gotils v0.0.0-20210921075833-21a6215cb0e4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
 )
 
 go 1.17
---
+++ routers/fasthttp-router.go
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+package routers
+
+import (
+	"github.com/fasthttp/router"
+	"github.com/valyala/fasthttp"
+
+	"github.com/caixw/go-http-routers-testing/apis"
+)
+
+func init() {
+	Routers = append(Routers, &Router{
+		Name: "fasthttp-router",
+		URL:  "https://github.com/fasthttp/router",
+		Load: fasthttpRouterLoad,
+	})
+}
+
+func fasthttpRouterLoad(as []*apis.API) ServeFunc {
+	r := router.New()
+	for _, api := range as {
+		r.Handle(api.Method, api.Brace, func(ctx *fasthttp.RequestCtx) {
+			ctx.Write(ctx.URI().Path())
+		})
+	}
+
+	return func(api *apis.API) string {
+		resp := fasthttp.AcquireResponse()
+		req := fasthttp.AcquireRequest()
+		req.Header.SetMethod(api.Method)
+		req.SetRequestURI(api.Test)
+
+		ctx := &fasthttp.RequestCtx{Request: *req, Response: *resp}
+		r.Handler(ctx)
+		body := ctx.Response.Body()
+
+		fasthttp.ReleaseRequest(req)
+		fasthttp.ReleaseResponse(resp)
+
+		return string(body)
+	}
+}
